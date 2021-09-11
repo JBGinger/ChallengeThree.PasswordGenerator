09e55bda20d6d4fb6ff4ac23492639a4b216f9e4
JBGinger <jeff.brittingham@protonmail.com>
2021-09-11 19:47:36 -0400

    Added password generator

--- Resources/script.js
+++ Resources/script.js
@@ -1,33 +1,48 @@
 // Assignment code here
-function passwordGenerator()
-{
-  //Define character types.
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 const numbers = "0123456789"
 const specialCharacters = "!@#$%^&*()_+-={}[];':<>/?|"
 
-//Ask user if they'd like to create a new password.
-var passwordCreation = confirm("Would you like to create a new password?");
-if (!passwordCreation)
+function passwordGenerator()
 {
-  return;
+  //Prompt user for password length and character types.
+  var passwordLength = prompt("How long do you want your password to be?");
+  if (passwordLength < 8 || passwordLength > 128)
+  {
+    alert("Password must be between 8 and 128 characters long.")
+    return;
+  }
+  else 
+  {
+    var includeUpperCase = confirm("Do you want your password to include uppercase letters?")
+    var includeLowerCase = confirm("Do you want your password to include lowercase letters?")
+    var includeNumbers = confirm("Do you want your password to include numbers?")
+    var includeSpecialCharacters = confirm("Do you want your password to include special characters?")
+  }
+
+  if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSpecialCharacters)
+  {
+    alert("Your password needs to include something!");
+    return;
+  }
+
+  //Generate password based on user responses.
+  initialpassword = "";
+  var totalSet = (includeUpperCase) ? upperCaseLetters : [];
+  totalSet = totalSet.concat((includeLowerCase) ? lowerCaseLetters : []);
+  totalSet = totalSet.concat((includeNumbers) ? numbers : []); 
+  totalSet = totalSet.concat((includeSpecialCharacters) ? specialCharacters : []); 
+  while (initialpassword.length < passwordLength)
+  {
+    initialpassword = initialpassword.concat(totalSet[getRandomInt(totalSet.length)]);
+  }
+  return initialpassword;
 }
 
-//Prompt user for password length and character types.
-var passwordLength = prompt("How long do you want your password to be?");
-if (passwordLength < 8 || passwordLength > 128)
+function getRandomInt(max)
 {
-  alert("Password must be between 8 and 128 characters long.")
-  return;
-} 
-else 
-{
-  var includeUpperCase = confirm("Do you want your password to contain uppercase letters?")
-  var includeLowerCase = confirm("Do you want your password to contain lowercase letters?")
-  var includeNumbers = confirm("Do you want your password to contain numbers?")
-  var includeSpecialCharacters = confirm("Do you want your password to contain special characters?")
-}
+  return Math.floor(Math.random() * max);
 }
 
 // Get references to the #generate element
@@ -37,9 +52,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = passwordGenerator();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = (password == undefined) ? "Error: password not specified" : password;
 }
 
 // Add event listener to generate button
